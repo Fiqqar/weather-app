24b84030f0f5cdeebba839d4b8b5630724ebcf07
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-05-12 11:30:19 +0700

    12-5-24 ( first commit )
    done
    -initialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' :''"></div>
+<main>
+  <div class="search-box">
+    <input 
+    type="text" 
+    placeholder="type to search for location"
+    v-model="query"
+    @keypress="fetchWeather"
+    class="search-bar"/>
+  </div>
+
+  <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+    <div class="location-box">
+    <div class="location">
+      {{ weather.name }}, {{ weather.sys.country }}
+    </div>
+    <div class="date">
+      {{ dateBuilder() }}
+    </div>  
+  </div>
+  </div>
+</main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      weather: {},
+      query: "",
+      api_key: '9336feb67e548b744acd07195e4dfa3d',
+      url_base: 'https://api.openweathermap.org/data/3.0/'
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+      if(e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query} &units=metric&APPID=${this.api_key}`).then (res => {
+          return res.json();
+        }).then (this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    }
   }
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
