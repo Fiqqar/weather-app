a753665a1e18ed0a11e0f1301596e6d9a2545bd6
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-05-17 20:32:15 +0700

    17-5-24 (completed)
    done
    adding styles, background for temperature, and a little bit of debugging

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' :''"></div>
+<div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 25 ? 'warm' :''">
 <main>
   <div class="search-box">
     <input 
@@ -10,17 +10,27 @@
     class="search-bar"/>
   </div>
 
-  <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+  <div div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
     <div class="location-box">
-    <div class="location">
-      {{ weather.name }}, {{ weather.sys.country }}
+      <div class="location">
+        {{ weather.name }}, {{ weather.sys.country }}
+      </div>
+      <div class="date">
+        {{ dateBuilder() }}
+      </div>  
     </div>
-    <div class="date">
-      {{ dateBuilder() }}
-    </div>  
-  </div>
+
+    <div class="weather-box">
+      <div class="temp">
+        {{Math.round( weather.main.temp )}}°C
+      </div>
+      <div class="weather">
+        {{ weather.weather[0].main }}
+      </div>
+    </div>
   </div>
 </main>
+</div>
 </template>
 
 <script>
@@ -31,7 +41,7 @@ export default {
       weather: {},
       query: "",
       api_key: '9336feb67e548b744acd07195e4dfa3d',
-      url_base: 'https://api.openweathermap.org/data/3.0/'
+      url_base: 'https://api.openweathermap.org/data/2.5/'
     }
   },
   methods: {
@@ -44,6 +54,18 @@ export default {
     },
     setResults(results) {
       this.weather = results;
+    },
+    dateBuilder() {
+      let D = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[D.getDay()];
+      let date = D.getDate();
+      let month = months[D.getMonth()];
+      let year = D.getFullYear();
+
+      return `${day}, ${date} ${month} ${year}`;
     }
   }
 }
@@ -52,5 +74,105 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+body {
+  font-family: "JetBrains Mono", monospace;
+}
 
+#app .warm {
+  background-image: url('./assets/warm-bg.jpg');
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 80px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
